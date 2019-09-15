18416c67cd57d0e51b801e508470e3491c664728
Shadowmaple <1142319190@qq.com>
2019-09-15 09:49:08 +0800

    init

---
+++ handler/handler.go
@@ -0,0 +1,48 @@
+package handler
+
+import (
+	"github.com/MuxiKeStack/muxiK-StackBackend/util"
+	"net/http"
+
+	"github.com/MuxiKeStack/muxiK-StackBackend/pkg/errno"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lexkong/log"
+	"github.com/lexkong/log/lager"
+)
+
+type Response struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func SendResponse(c *gin.Context, err error, data interface{}) {
+	code, message := errno.DecodeErr(err)
+
+	c.JSON(http.StatusOK, Response{
+		Code:    code,
+		Message: message,
+		Data:    data,
+	})
+}
+
+func SendBadRequest(c *gin.Context, err error, data interface{}, cause string) {
+	code, message := errno.DecodeErr(err)
+	log.Info(message, lager.Data{"X-Request-Id": util.GetReqID(c), "cause": cause})
+	c.JSON(http.StatusBadRequest, Response{
+		Code:    code,
+		Message: message + ": " + cause,
+		Data:    data,
+	})
+}
+
+func SendError(c *gin.Context, err error, data interface{}, cause string) {
+	code, message := errno.DecodeErr(err)
+	log.Info(message, lager.Data{"X-Request-Id": util.GetReqID(c), "cause": cause})
+	c.JSON(http.StatusInternalServerError, Response{
+		Code:    code,
+		Message: message + ": " + cause,
+		Data:    data,
+	})
+}
---
+++ handler/user/list.go
@@ -0,0 +1,29 @@
+package user
+
+import (
+	. "github.com/MuxiKeStack/muxiK-StackBackend/handler"
+	"github.com/MuxiKeStack/muxiK-StackBackend/pkg/errno"
+	"github.com/MuxiKeStack/muxiK-StackBackend/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+// List list the users in the database.
+func List(c *gin.Context) {
+	var r ListRequest
+	if err := c.Bind(&r); err != nil {
+		SendBadRequest(c, errno.ErrBind, nil, err.Error())
+		return
+	}
+
+	infos, count, err := service.ListUser(r.Username, r.Offset, r.Limit)
+	if err != nil {
+		SendError(c, err, nil, err.Error())
+		return
+	}
+
+	SendResponse(c, nil, ListResponse{
+		TotalCount: count,
+		UserList:   infos,
+	})
+}
