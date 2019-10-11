04fe9a07d01ea5539a4e9532cbf4df6570160740
Bowser1704 <yuqlang@163.com>
2019-10-11 16:18:48 +0800

    edit auth/login

--- handler/user/user.go
+++ handler/user/user.go
@@ -19,13 +19,8 @@ type ListRequest struct {
 	Limit    int    `json:"limit"`
 }
 
-type ListResponse struct {
-	TotalCount uint64            `json:"totalCount"`
-	UserList   []*model.UserInfo `json:"userList"`
-}
-
 type LoginResponse struct {
-	Code    int         `json:"code"`
-	Message string      `json:"message"`
-	Data    model.Token `json:"data"`
+	Code    int                `json:"code"`
+	Message string             `json:"message"`
+	Data    model.AuthResponse `json:"data"`
 }
--- model/user.go
+++ model/user.go
@@ -1,34 +1,31 @@
 package model
 
 import (
-	"fmt"
-	"github.com/MuxiKeStack/muxiK-StackBackend/pkg/auth"
-	"github.com/MuxiKeStack/muxiK-StackBackend/pkg/constvar"
-
-	validator "gopkg.in/go-playground/validator.v9"
+	"gopkg.in/go-playground/validator.v9"
 )
-
 
 func (c *UserModel) TableName() string {
 	return "user"
 }
 
 // Create creates a new user account.
-func (u *UserModel) Create() error {
-	return DB.Self.Create(&u).Error
+func CreateUser(sid uint64) error {
+	return DB.Self.Create(&UserModel{Sid: sid}).Error
 }
-
 
 // Update updates an user account information.
 func (u *UserModel) Update() error {
 	return DB.Self.Save(u).Error
 }
 
-// GetUser gets an user by the user identifier.
-func GetUser(username string) (*UserModel, error) {
-	u := &UserModel{}
-	d := DB.Self.Where("username = ?", username).First(&u)
-	return u, d.Error
+// HaveUser determines whether there is this user or not by the user identifier.
+func HaveUser(sid uint64) (uint8, error) {
+	var num int
+	DB.Self.Model(&UserModel{}).Where("sid = ?", sid).Count(num)
+	if num == 0 {
+		return 0, nil
+	}
+	return 1, nil
 }
 
 // GetUser gets an user by the user identifier.
@@ -36,39 +33,6 @@ func GetUserById(id uint64) (*UserModel, error) {
 	u := &UserModel{}
 	d := DB.Self.Where("id = ?", id).First(&u)
 	return u, d.Error
-}
-
-// ListUser List all users
-func ListUser(username string, offset, limit int) ([]*UserModel, uint64, error) {
-	if limit == 0 {
-		limit = constvar.DefaultLimit
-	}
-
-	users := make([]*UserModel, 0)
-	var count uint64
-
-	where := fmt.Sprintf("username like '%%%s%%'", username)
-	if err := DB.Self.Model(&UserModel{}).Where(where).Count(&count).Error; err != nil {
-		return users, count, err
-	}
-
-	if err := DB.Self.Where(where).Offset(offset).Limit(limit).Order("id desc").Find(&users).Error; err != nil {
-		return users, count, err
-	}
-
-	return users, count, nil
-}
-
-// Compare with the plain text password. Returns true if it's the same as the encrypted one (in the `User` struct).
-func (u *UserModel) Compare(pwd string) (err error) {
-	err = auth.Compare(u.Password, pwd)
-	return
-}
-
-// Encrypt the user password.
-func (u *UserModel) Encrypt() (err error) {
-	u.Password, err = auth.Encrypt(u.Password)
-	return
 }
 
 // Validate the fields.
