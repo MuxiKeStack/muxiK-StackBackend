eefa15cebacf52d7e1da65f57f602d27a32513df
Shadowmaple <1142319190@qq.com>
2019-10-11 17:55:04 +0800

    update model for comment and evaluation

--- model/commentModel.go
+++ model/commentModel.go
@@ -1,20 +1,47 @@
 package model
 
-// 评课
-type CourseCommentTable struct {
-	CourseCommentId string   `json:"course_comment_id"` // 评课id
-	CourseName      string   `json:"course_name"`       // 课程名称
-	Attendance      string   `json:"attendance"`        // 考勤方式
-	ExamWay         string   `json:"exam_way"`          // 考核方式
-	Content         string   `json:"content"`           // 评课内容
-	IsAnonymous     bool     `json:"is_anonymous"`      // 是否匿名
-	LikeNum         int64    `json:"like_num"`          // 点赞数
-	Star            int64    `json:"star"`              // 评价星级
-	CommentNum      int64    `json:"comment_num"`       // 一级评论数
-	LikeUsers       []string `json:"like_users"`        // 点赞用户（用户id）
+// 评课物理表
+type CourseEvaluationModel struct {
+	Id 					uint64 `gorm:"column:id; primary_key; AUTO_INCREMENT"`
+	CourseId 			string `gorm:"column:course_id"` 				// 课程id
+	CourseName      	string `gorm:"column:course_name"`       		// 课程名称
+	UserId				uint64 `gorm:"column:user_id"` 					// 用户id
+	Rate            	uint8  `gorm:"column:rate"`              		// 评价星级
+	AttendanceCheckType uint8  `gorm:"column:attendance_check_type"` 	// 考勤方式
+	ExamCheckType       uint8  `gorm:"column:exam_check_type"`      	// 考核方式
+	Content         	string `gorm:"column:content"`           		// 评课内容
+	LikeNum         	int64  `gorm:"column:like_num"`          		// 点赞数
+	CommentNum      	int64  `gorm:"column:comment_num"`       		// 一级评论数
+	Tags 				string `gorm:"column:tags"` 					// 标签id列表，逗号分隔
+	IsAnonymous     	bool   `gorm:"column:is_anonymous"`      		// 是否匿名
+	IsValid 			bool   `gorm:"column:is_valid"`					// 是否有效，未被折叠
+	Time 				string `gorm:"column:time"`						// 时间，时间戳
 }
 
-// 评论
-type CommentTable struct {
+// 评论物理表
+type CommentModel struct {
+	Id				uint64 `gorm:"column:id; primary_key; AUTO_INCREMENT"`
+	UserId			uint64 `gorm:"column:user_id"`
+	ParentId		uint64 `gorm:"column:parent_id"`
+	CommentTargetId uint64 `gorm:"column:comment_target_id"`
+	Content			string `gorm:"column:content"`
+	LikeNum			uint64 `gorm:"column:like_num"`
+	IsRoot			bool   `gorm:"column:is_root"`
+	Time			string `gorm:"column:time"`
+	SubCommentNum	uint64 `gorm:"column:sub_comment_num"`
+}
+
+// 发布评课
+type EvaluationPublish struct {
+
+}
+
+// 评课信息
+type EvaluationInfo struct {
+
+}
+
+// 评论信息
+type CommentInfo struct {
 
 }
