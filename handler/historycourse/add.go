d35fdd5067b09896f49cbdf992f6fc160d1e5e02
hjm1027 <44267290+hjm1027@users.noreply.github.com>
2019-10-23 22:06:17 +0800

    add course

---
+++ handler/historycourse/add.go
@@ -0,0 +1,15 @@
+package course
+
+import (
+	"github.com/MuxiKeStack/muxiK-StackBackend/handler"
+	"github.com/MuxiKeStack/muxiK-StackBackend/model"
+	"github.com/MuxiKeStack/muxiK-StackBackend/pkg/errno"
+	"github.com/MuxiKeStack/muxiK-StackBackend/pkg/token"
+
+	"github.com/gin-gonic/gin"
+)
+
+//增加一个历史课程
+func AddHistoryCourse(c *gin.Context) {
+
+}
---
+++ handler/historycourse/query.go
@@ -0,0 +1,15 @@
+package course
+
+import (
+	"github.com/MuxiKeStack/muxiK-StackBackend/handler"
+	"github.com/MuxiKeStack/muxiK-StackBackend/model"
+	"github.com/MuxiKeStack/muxiK-StackBackend/pkg/errno"
+	"github.com/MuxiKeStack/muxiK-StackBackend/pkg/token"
+
+	"github.com/gin-gonic/gin"
+)
+
+//查询历史课程
+func QueryHistoryCourse(c *gin.Context) {
+
+}
