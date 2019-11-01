2b9c7ee2941045e680600c99c2c4d840bdbd4ccb
Shadowmaple <1142319190@qq.com>
2019-11-01 12:57:56 +0800

    to resolve conflicts

--- pkg/errno/code.go
+++ pkg/errno/code.go
@@ -9,6 +9,7 @@ var (
 	ErrValidation = &Errno{Code: 20001, Message: "Validation failed."}
 	ErrDatabase   = &Errno{Code: 20002, Message: "Database error."}
 	ErrToken      = &Errno{Code: 20003, Message: "Error occurred while signing the JSON web token."}
+	ErrDelete     = &Errno{Code: 20004, Message: "Do not have this delete target"}
 
 	// Auth errors
 	ErrAuthFailed   = &Errno{Code: 20101, Message: "The sid or password was incorrect."}
@@ -19,4 +20,10 @@ var (
 	ErrUpdateUser   = &Errno{Code: 20202, Message: "Error in updating user"}
 	ErrUserNotFound = &Errno{Code: 20203, Message: "The user was not found."}
 	ErrGetUserInfo  = &Errno{Code: 20204, Message: "Error in getting user info"}
+
+	// table errors
+	ErrTableExisting = &Errno{Code: 20401, Message: "The table is not existing "}
+	ErrClassExisting = &Errno{Code: 20402, Message: "The class is not existing "}
+	ErrClassIdRequired = &Errno{Code: 20403, Message: "The classId is required "}
+	ErrGetClassInfo = &Errno{Code: 20404, Message: "Error occurred in getting class info"}
 )
