6600808be7f6b8d7c512b11328069606a818ca16
hjm1027 <44267290+hjm1027@users.noreply.github.com>
2019-11-08 14:12:59 +0800

    get info

--- handler/course/getinfo.go
+++ handler/course/getinfo.go
@@ -11,5 +11,17 @@ import (
 
 //获取课程信息
 func GetCourseInfo(c *gin.Context) {
+	courseId, err := strconv.ParseUint(c.Param("courseId"), 10, 32)
+	if err != nil {
+		handler.SendBadRequest(c, errno.ErrGetParam, nil, err.Error())
+		return
+	}
+	
+	courseInfo, err := service.GetCourseInfo(courseid)
+	if err != nil {
+		handler.SendError(c, err, nil, err.Error())
+		return
+	}
 
+	handler.SendResponse(c, nil, courseInfo)
 }
