98bf6a06c367e8da01197c7977fcee513d51b4d3
hjm1027 <44267290+hjm1027@users.noreply.github.com>
2019-11-19 22:46:14 +0800

    course.go

---
+++ service/course.go
@@ -0,0 +1,15 @@
+package service
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/MuxiKeStack/muxiK-StackBackend/model"
+
+	"github.com/lexkong/log"
+)
+
+// Get course info.
+func CourseList(id, userId uint32) (*model.CourseInfo, error) {
+
+}
