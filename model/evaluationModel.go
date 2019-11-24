6e4a0e1f5f2f781d09c1efdcca77080d93f9569b
Shadowmaple <1142319190@qq.com>
2019-11-24 21:00:28 +0800

    convert pageNum to page

--- model/evaluationModel.go
+++ model/evaluationModel.go
@@ -4,19 +4,19 @@ import "time"
 
 // 评课物理表
 type CourseEvaluationModel struct {
-	Id                  uint32  `gorm:"column:id; primary_key; AUTO_INCREMENT"`
-	CourseId            string  `gorm:"column:course_id"`             // 课程id
-	CourseName          string  `gorm:"column:course_name"`           // 课程名称
-	UserId              uint32  `gorm:"column:user_id"`               // 用户id
-	Rate                float32 `gorm:"column:rate"`                  // 评价星级
-	AttendanceCheckType uint8   `gorm:"column:attendance_check_type"` // 考勤方式
-	ExamCheckType       uint8   `gorm:"column:exam_check_type"`       // 考核方式
-	Content             string  `gorm:"column:content"`               // 评课内容
-	LikeNum             uint32  `gorm:"colum:like_num"`               // 点赞数
-	CommentNum          uint32  `gorm:"column:comment_num"`           // 一级评论数
-	Tags                string  `gorm:"column:tags"`                  // 标签id列表，逗号分隔
-	IsAnonymous         bool    `gorm:"column:is_anonymous"`          // 是否匿名
-	IsValid             bool    `gorm:"column:is_valid"`              // 是否有效，未被折叠
+	Id                  uint32     `gorm:"column:id; primary_key; AUTO_INCREMENT"`
+	CourseId            string     `gorm:"column:course_id"`             // 课程id
+	CourseName          string     `gorm:"column:course_name"`           // 课程名称
+	UserId              uint32     `gorm:"column:user_id"`               // 用户id
+	Rate                float32    `gorm:"column:rate"`                  // 评价星级
+	AttendanceCheckType uint8      `gorm:"column:attendance_check_type"` // 考勤方式
+	ExamCheckType       uint8      `gorm:"column:exam_check_type"`       // 考核方式
+	Content             string     `gorm:"column:content"`               // 评课内容
+	LikeNum             uint32     `gorm:"colum:like_num"`               // 点赞数
+	CommentNum          uint32     `gorm:"column:comment_num"`           // 一级评论数
+	Tags                string     `gorm:"column:tags"`                  // 标签id列表，逗号分隔
+	IsAnonymous         bool       `gorm:"column:is_anonymous"`          // 是否匿名
+	IsValid             bool       `gorm:"column:is_valid"`              // 是否有效，未被折叠
 	Time                *time.Time `gorm:"column:time"`                  // 时间，时间戳
 }
 
@@ -46,15 +46,3 @@ type EvaluationInfo struct {
 	UserInfo            *UserInfoResponse `json:"user_info"`
 	IsValid             bool              `json:"is_valid"`
 }
-
-//type EvaInfos []EvaluationInfo
-//
-//func (infos EvaInfos) Len() int { return len(infos) }
-//
-//func (infos EvaInfos) Less(i, j int) bool {
-//	return infos[i].LikeNum > infos[j].LikeNum
-//}
-//
-//func (infos EvaInfos) Swap(i, j int) {
-//	infos[i], infos[j] = infos[j], infos[i]
-//}
