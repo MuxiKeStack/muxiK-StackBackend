f1a96bc9e3557c2ebcc25252b78e32e14761bbd6
hjm1027 <44267290+hjm1027@users.noreply.github.com>
2019-11-28 20:58:23 +0800

    modify

--- model/courseModel.go
+++ model/courseModel.go
@@ -5,7 +5,7 @@ type HistoryCourseModel struct {
 	Id       uint32  `gorm:"column:id; primary_key"`
 	Hash     string  `gorm:"column:hash; unique_key"` //教师名和课程hash成的唯一标识
 	Name     string  `gorm:"column:name"`             //课程名称
-	Teacher  string  `gorm:"column:teacher"`          //教师姓名
+	Teacher  string  `gorm:"column:teacher"`          //教师性名
 	Type     uint8   `gorm:"column:type"`             //课程类型，公共课为0，专业课为1
 	Rate     float32 `gorm:"column:rate"`             //课程评价星级
 	StarsNum uint32  `gorm:"column:stars_num"`        //参与评分人数
@@ -14,45 +14,34 @@ type HistoryCourseModel struct {
 
 //选课手册课堂物理表
 type UsingCourseModel struct {
-	Id            uint32  `gorm:"column:id; primary_key"`
-	Hash          string  `gorm:"column:hash; unique_key"` //教师名和课程hash成的唯一标识
-	Name          string  `gorm:"column:name"`             //课程名称
-	Credit        float32 `gorm:"column:credit"`           //学分
-	Teacher       string  `gorm:"column:teacher"`          //教师性名
-	CourseId      string  `gorm:"column:course_id"`        //课程号, UI上需要展示
-	ClassId       uint64  `gorm:"column:class_id"`         //课堂编号，用于区分课堂
+	Id       uint32  `gorm:"column:id; primary_key"`
+	Hash     string  `gorm:"column:hash; unique_key"` //教师名和课程hash成的唯一标识
+	Name     string  `gorm:"column:name"`             //课程名称
+	Credit   float32 `gorm:"column:credit"`           //学分
+	Teacher  string  `gorm:"column:teacher"`          //教师性名
+	CourseId uint64  `gorm:"column:course_id"`        //UI上需要展示
+	ClassId  uint64  `gorm:"column:class_id"`         //课堂编号，用于区分课堂
 	Type          uint8   `gorm:"column:type"`             //通识必修，通识选修，通识核心，专业必修，专业选修分别为 0/1/2/3/4
-	CreditType    uint8   `gorm:"column:credit_type"`      //学分类别，文科理科艺术之类的，加索引（筛选条件）
-	TotalScore    float32 `gorm:"column:total_score"`      //总评均分
-	OrdinaryScore float32 `gorm:"column:ordinary_score"`   //平时均分
-	Time1         string  `gorm:"column:time1"`            //上课时间1
-	Place1        string  `gorm:"column:place1"`           //上课地点1
-	Time2         string  `gorm:"column:time2"`            //上课时间2
-	Place2        string  `gorm:"column:place2"`           //上课地点2
-	Time3         string  `gorm:"column:time3"`            //上课时间3
-	Place3        string  `gorm:"column:place3"`           //上课地点3
-	Weeks1        string  `gorm:"column:weeks1"`           //上课周数
-	Weeks2        string  `gorm:"column:weeks2"`           //上课周数
-	Weeks3        string  `gorm:"column:weeks3"`           //上课周数
-	Region        uint8   `gorm:"column:region"`           //上课地区，南湖，东区，西区。加索引（筛选条件）
-}
-
-// 用户课堂表
-type CourseList struct {
-	Id           uint32 `gorm:"column:id; primary_key"` //主键
-	UserId       uint32 `gorm:"column:user_id"`         //用户ID
-	CourseHashId string `gorm:"column:course_hash_id"`  //课堂HashID
+	Time1  string `gorm:"column:time1"`  //上课时间1
+	Place1 string `gorm:"column:place1"` //上课地点1
+	Time2  string `gorm:"column:time2"`  //上课时间2
+	Place2 string `gorm:"column:place2"` //上课地点2
+	Time3  string `gorm:"column:time3"`  //上课时间3
+	Place3 string `gorm:"column:place3"` //上课地点3
+	Weeks1 string `gorm:"column:weeks1"` //上课周数
+	Weeks2 string `gorm:"column:weeks2"` //上课周数
+	Weeks3 string `gorm:"column:weeks3"` //上课周数
+	Region uint8  `gorm:"column:region"` //上课地区，暂定：东区，西区，其他。加索引（筛选条件）
 }
 
 type HistoryCourseInfo struct {
+	Hash              string
 	CourseId      string
 	Name          string
 	Teacher       string
 	Type          uint8
 	Rate          float32
 	StarsNum      uint32
-	TotalScore    float32
-	OrdinaryScore float32
 }
 
 type UsingCourseInfo struct {
@@ -62,13 +51,14 @@ type UsingCourseInfo struct {
 	Credit     float32
 	Teacher    string
 	Type       uint8
-	CreditType uint8
 	Time1      string
 	Place1     string
 	Time2      string
 	Place2     string
 	Time3      string
 	Place3     string
-	Weeks      string
+	Weeks1     string
+	Weeks2     string
+	Weeks3     string
 	Region     uint8
 }
