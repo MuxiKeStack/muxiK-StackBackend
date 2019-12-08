0f97454f436877071e641dbb768c28826fd8d910
hjm1027 <44267290+hjm1027@users.noreply.github.com>
2019-12-08 10:13:01 +0800

    add info

--- handler/course/getinfo.go
+++ handler/course/getinfo.go
@@ -1,28 +1,32 @@
 package course
 
 import (
+	"strconv"
+
 	"github.com/MuxiKeStack/muxiK-StackBackend/handler"
-	_ "github.com/MuxiKeStack/muxiK-StackBackend/model"
+	"github.com/MuxiKeStack/muxiK-StackBackend/model"
 	"github.com/MuxiKeStack/muxiK-StackBackend/pkg/errno"
 	_ "github.com/MuxiKeStack/muxiK-StackBackend/pkg/token"
 	_ "github.com/MuxiKeStack/muxiK-StackBackend/service"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lexkong/log"
 )
 
 //获取课程信息
 func GetCourseInfo(c *gin.Context) {
-	courseId := c.DefaultQuery("courseId", "")
-	if courseId == "" || len(courseId) != 8 {
-		handler.SendBadRequest(c, errno.ErrGetParam, nil, "CourseId must be required and length must be 8")
+	//id := c.Param("id")
+	id, err := strconv.ParseUint(c.Param("id"), 10, 32)
+	id32 := uint32(id)
+	if err != nil {
+		handler.SendBadRequest(c, errno.ErrGetParam, nil, err.Error())
+		return
+	}
+	course := &model.UsingCourseModel{Id: id32}
+	if err := course.GetById(); err != nil {
+		log.Info("course.GetById() error.")
 		return
 	}
 
-	// courseInfo, err := service.GetCourseInfo(courseid)
-	// if err != nil {
-	// 	handler.SendError(c, err, nil, err.Error())
-	// 	return
-	// }
-
-	// handler.SendResponse(c, nil, courseInfo)
+	handler.SendResponse(c, nil, course)
 }
