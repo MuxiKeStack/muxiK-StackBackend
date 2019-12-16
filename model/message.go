03fc7ac477207231c002e817dbd13cdbd3c5886c
Bowser1704 <yuqlang@163.com>
2019-12-16 09:22:09 +0800

    <fix>: fix message

--- model/message.go
+++ model/message.go
@@ -38,8 +38,7 @@ func GetCount(uid uint32) (uint32, error) {
 }
 
 func ReadAll(uid uint32) error {
-	var messages []Message
-	d := DB.Self.Table("message").Where("sub_user_id = ? AND is_read = ?", uid, '0').Find(&messages)
-	d = DB.Self.Model(&messages).Update("is_read", 1)
+	d := DB.Self.Table("message").Where("sub_user_id = ?", uid).Update("is_read", 1)
+	// d = DB.Self.Model(&messages).Update("is_read", 1)
 	return d.Error
 }
