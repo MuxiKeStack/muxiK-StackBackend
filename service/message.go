a2ebe82c59254d47017bd86b030ed59a26890404
Bowser1704 <yuqlang@163.com>
2020-01-16 11:56:26 +0800

    <fix> fix message

--- service/message.go
+++ service/message.go
@@ -11,6 +11,7 @@ import (
 )
 
 /*
+系统提醒的用户id为 0或者1<<31
 对消息提醒的整理，分为三种，评论，点赞，举报，用kind来表识。
 所有消息提醒都有一个tag，即为MessageInfo，信息有课程名，课程老师名，课程ID
 所有的点击都是返回评课id，所以必须要有的id是前两个。
@@ -266,8 +267,69 @@ func NewMessageForSubCommentLiking(userId uint32, comment *model.SubCommentModel
 	return nil
 }
 
-func NewMessageForReport(userId uint32) {}
+func NewMessageForReport(userId uint32, evaluationId uint32) error {
+	evaluation := &model.CourseEvaluationModel{Id: evaluationId}
+	if err := evaluation.GetById(); err != nil {
+		log.Info("evaluation.GetById function error")
+		return err
+	}
 
-func NewMessageForSystem(userId uint32, CourseId string) {
+	teacher, err := model.GetTeacherByCourseId(evaluation.CourseId)
+	if err != nil {
+		log.Info("GetTeacherByCourseId function error")
+		return err
+	}
 
+	message := &model.MessagePub{
+		PubUserId:       (uint32)(1 << 31),
+		SubUserId:       userId,
+		Kind:            2, //举报
+		IsRead:          false,
+		Reply:           "你的评价被多人举报已被删除",
+		Time:            strconv.FormatInt(time.Now().Unix(), 10),
+		CourseId:        evaluation.CourseId,
+		CourseName:      evaluation.CourseName,
+		Teacher:         teacher,
+		EvaluationId:    evaluation.Id,
+		Content:         "",
+		Sid:             "",
+		ParentCommentId: "",
+	}
+
+	err = model.CreateMessage(message)
+	if err != nil {
+		log.Info("CreateMessage function error")
+		return err
+	}
+	return nil
+}
+
+func NewMessageForSystem(userId uint32, usingCourseId uint32) error {
+	usingCourse := &model.UsingCourseModel{Id: usingCourseId}
+	if err := usingCourse.GetById(); err != nil {
+		log.Info("GetCourseByCourseId function error")
+		return err
+	}
+	message := &model.MessagePub{
+		PubUserId:       (uint32)(1 << 31),
+		SubUserId:       userId,
+		Kind:            3, //系统提醒
+		IsRead:          false,
+		Reply:           "你的课程还未评课",
+		Time:            strconv.FormatInt(time.Now().Unix(), 10),
+		CourseId:        usingCourse.CourseId,
+		CourseName:      usingCourse.Name,
+		Teacher:         usingCourse.Teacher,
+		EvaluationId:    0,
+		Content:         "",
+		Sid:             "",
+		ParentCommentId: "",
+	}
+
+	err := model.CreateMessage(message)
+	if err != nil {
+		log.Info("CreateMessage function error")
+		return err
+	}
+	return nil
 }
