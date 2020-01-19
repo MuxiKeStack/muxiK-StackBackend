af97d6610accc4f00282dedbc14cd01097445714
hjm1027 <1749511631@qq.com>
2020-01-19 10:01:15 +0800

    some fix

--- model/tagModel.go
+++ model/tagModel.go
@@ -8,6 +8,6 @@ type TagModel struct {
 type CourseTagModel struct {
 	Id       uint32 `gorm:"column:id"`
 	TagId    uint32 `gorm:"column:tag_id"`
-	CourseId string `gorm:"column:course_id"`
+	CourseId string `gorm:"column:course_id"` //此处的CourseId实际上是hash
 	Num      uint32 `gorm:"column:num"`
 }
